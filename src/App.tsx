6f65a1010cb9c121057edd052f6a8a654ce0f1fc
Nikola Occidentale <nikolaoccid@gmail.com>
2023-04-17 15:26:01 +0200

    Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
