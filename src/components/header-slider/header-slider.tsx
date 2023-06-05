56edf55bc6c1b775b82b168404980b83beb3b235
Nikola Occidentale <nikolaoccid@gmail.com>
2023-06-05 15:59:19 +0200

    Fix article header link

--- src/components/header-slider/header-slider.tsx
+++ src/components/header-slider/header-slider.tsx
@@ -6,9 +6,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 
+import articleAuthor from '/article-author.png';
+import articleImageBody from '/article-book.png';
 import heroImage from '/article-hero.png';
+import articleHero from '/article-hero.png';
 import commentIcon from '/comment.png';
 
+import { articleNameDashes } from '../../utils/article-name-dashes';
 import articleMountainImage from './assets/carousel-bg.png';
 import { LeftArrow } from './components/left-arrow/left-arrow';
 import { RightArrow } from './components/right-arrow/right-arrow';
@@ -105,7 +109,24 @@ export const HeaderSlider = () => {
               </CommentMetaData>
             </MetaData>
             <TitleText>Palestinians call off peace talks after clash</TitleText>
-            <CtaButton to="">Read more</CtaButton>
+            <CtaButton
+              to={`/article/${articleNameDashes('Palestinians call off peace talks after clash')}`}
+              state={{
+                title: 'Palestinians call off peace talks after clash',
+                date: 'May 26th, 2023',
+                bodyImage: articleImageBody,
+                heroImage: articleHero,
+                firstParagraph:
+                  'Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis. Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna mollis ornare vel eu leo.',
+                secondParagraph:
+                  'Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec hac ultrices nulla non netus. Placerat vehicula donec non suscipit egestas, augue vel suspendisse. Et felis venenatis blandit sed est ultrices, adipiscing urna, at aliquam nullam facilisis aliquet sapien, eget duis consectetuer tristique nunc vitae erat, mi purus nisl lorem. Ac magna lobortis non, vulputate vitae viverra. Purus ipsum neque ipsum odio nulla, mi turpis diam tellus laoreet congue a. Rhoncus maecenas, sit suspendisse, condimentum purus convallis dui hendrerit. Purus ipsum neque ipsum odio nulla, mi turpis diam tellus laoreet congue.',
+                authorImage: articleAuthor,
+                authorDescription:
+                  'Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec hac ultrices nulla non netus. Placerat vehicula donec non suscipit egestas, augue vel suspendisse. Et felis venenatis blandit sed est ultrices, adipiscing urna, at aliquam nullam facilisis aliquet sapien, eget duis consectetuer tristique nunc vitae erat, mi purus nisl lorem.',
+              }}
+            >
+              Read more
+            </CtaButton>
           </HeroText>
         </Slide>
 
@@ -120,7 +141,24 @@ export const HeaderSlider = () => {
               </CommentMetaData>
             </MetaData>
             <TitleText>&apos;Margot&apos; breathlessly reimagines Anne Frank&apos;s sister</TitleText>
-            <CtaButton to="">Read more</CtaButton>
+            <CtaButton
+              to={`/article/${articleNameDashes("'Margot' breathlessly reimagines Anne Frank's sister")}`}
+              state={{
+                title: "'Margot' breathlessly reimagines Anne Frank's sister",
+                date: 'May 26th, 2023',
+                bodyImage: articleImageBody,
+                heroImage: articleHero,
+                firstParagraph:
+                  'Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis. Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna mollis ornare vel eu leo.',
+                secondParagraph:
+                  'Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec hac ultrices nulla non netus. Placerat vehicula donec non suscipit egestas, augue vel suspendisse. Et felis venenatis blandit sed est ultrices, adipiscing urna, at aliquam nullam facilisis aliquet sapien, eget duis consectetuer tristique nunc vitae erat, mi purus nisl lorem. Ac magna lobortis non, vulputate vitae viverra. Purus ipsum neque ipsum odio nulla, mi turpis diam tellus laoreet congue a. Rhoncus maecenas, sit suspendisse, condimentum purus convallis dui hendrerit. Purus ipsum neque ipsum odio nulla, mi turpis diam tellus laoreet congue.',
+                authorImage: articleAuthor,
+                authorDescription:
+                  'Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec hac ultrices nulla non netus. Placerat vehicula donec non suscipit egestas, augue vel suspendisse. Et felis venenatis blandit sed est ultrices, adipiscing urna, at aliquam nullam facilisis aliquet sapien, eget duis consectetuer tristique nunc vitae erat, mi purus nisl lorem.',
+              }}
+            >
+              Read more
+            </CtaButton>
           </HeroText>
         </Slide>
       </Slider>
