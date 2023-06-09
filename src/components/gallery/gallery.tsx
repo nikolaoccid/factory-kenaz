f43f66715fc82b733fe1c3eba1197de9a5d2e486
Nikola Occidentale <nikolaoccid@gmail.com>
2023-06-09 14:14:13 +0200

    Gallery draft

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -2,6 +2,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import styled from '@emotion/styled';
+import FsLightbox from 'fslightbox-react';
 import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 
@@ -13,17 +14,16 @@ import gallery05 from '/gallery05.png';
 import gallery06 from '/gallery06.png';
 import gallery07 from '/gallery07.png';
 import gallery08 from '/gallery08.png';
-
+import magnifier from '/magnifier.png';
+const images: string[] = [gallery01, gallery02, gallery03, gallery04, gallery05, gallery06, gallery07, gallery08];
 import { LeftArrow } from '../header-slider/components/left-arrow/left-arrow';
 import { RightArrow } from '../header-slider/components/right-arrow/right-arrow';
 const Container = styled.div`
-  //max-width: 100vw;
   width: 940px;
 `;
 const ThumbnailImage = styled.img`
   width: 120px;
   height: 120px;
-  //margin-left: 10px;
 `;
 const MainGallery = styled(Slider)`
   max-width: 940px;
@@ -40,12 +40,26 @@ const NavSlider = styled(Slider)`
     margin: 0;
   }
 `;
+const ToggleButton = styled.button`
+  cursor: pointer;
+  border: 0;
+  padding: 0;
+  background: transparent;
+`;
+const FullScreenContainer = styled.div`
+  //position: relative;
+`;
+const TogglerImg = styled.img`
+  //position: absolute;
+`;
 export const Gallery = () => {
   const [nav1, setNav1] = useState<any>(null);
   const [nav2, setNav2] = useState<any>(null);
   const [slider1, setSlider1] = useState<any>(null);
   const [slider2, setSlider2] = useState<any>(null);
-
+  const [toggler, setToggler] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(1);
+  console.log('currentSlide', currentSlide);
   const settingsMain = {
     dots: false,
     slidesToShow: 1,
@@ -69,66 +83,41 @@ export const Gallery = () => {
     setNav1(slider1);
     setNav2(slider2);
   });
-
   return (
     <Container>
       <>
-        <MainGallery {...settingsMain} asNavFor={nav2} ref={(slider) => setSlider1(slider)}>
-          <div>
-            <img src={gallery01} alt="Gallery image" />
-          </div>
-
-          <div>
-            <img src={gallery02} alt="Gallery image" />
-          </div>
-          <div>
-            <img src={gallery03} alt="Gallery image" />
-          </div>
-          <div>
-            <img src={gallery04} alt="Gallery image" />
-          </div>
-          <div>
-            <img src={gallery05} alt="Gallery image" />
-          </div>
-          <div>
-            <img src={gallery06} alt="Gallery image" />
-          </div>
-          <div>
-            <img src={gallery07} alt="Gallery image" />
-          </div>
-          <div>
-            <img src={gallery08} alt="Gallery image" />
-          </div>
+        <MainGallery
+          {...settingsMain}
+          asNavFor={nav2}
+          ref={(slider) => setSlider1(slider)}
+          afterChange={setCurrentSlide}
+        >
+          {images.map((image) => (
+            <div key={image}>
+              <TogglerImg src={image} alt="Gallery image" />
+            </div>
+          ))}
         </MainGallery>
       </>
       <>
-        <NavSlider {...settingsThumbs} asNavFor={nav1} ref={(slider) => setSlider2(slider)}>
-          <div>
-            <ThumbnailImage src={gallery01} alt="Gallery image" />
-          </div>
-
-          <div>
-            <ThumbnailImage src={gallery02} alt="Gallery image" />
-          </div>
-          <div>
-            <ThumbnailImage src={gallery03} alt="Gallery image" />
-          </div>
-          <div>
-            <ThumbnailImage src={gallery04} alt="Gallery image" />
-          </div>
-          <div>
-            <ThumbnailImage src={gallery05} alt="Gallery image" />
-          </div>
-          <div>
-            <ThumbnailImage src={gallery06} alt="Gallery image" />
-          </div>
-          <div>
-            <ThumbnailImage src={gallery07} alt="Gallery image" />
-          </div>
-          <div>
-            <ThumbnailImage src={gallery08} alt="Gallery image" />
-          </div>
+        <NavSlider
+          {...settingsThumbs}
+          asNavFor={nav1}
+          ref={(slider) => setSlider2(slider)}
+          afterChange={setCurrentSlide}
+        >
+          {images.map((image) => (
+            <div key={image}>
+              <ThumbnailImage src={image} alt="Gallery image" />
+            </div>
+          ))}
         </NavSlider>
+        <FullScreenContainer>
+          <ToggleButton onClick={() => setToggler(!toggler)}>
+            <img src={magnifier} alt="Slider full screen icon" />
+          </ToggleButton>
+          <FsLightbox toggler={toggler} sources={[`/gallery0${currentSlide + 1}.png`, ...images]} />
+        </FullScreenContainer>
       </>
     </Container>
   );
